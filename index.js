b969fa6917c32bd78b9d41a7394aab56270df16c
DeeBest <simphiwedladla8@gmail.com>
2024-04-29 16:01:02 +0200

    created function to start the timer

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
 document.getElementById(
   'footer-paragraph'
 ).textContent = `Created By \xA9Simphiwe ${new Date().getFullYear()}.`;
+
+const timer = document.getElementById('time');
+const startButton = document.getElementById('start-button');
+const stopButton = document.getElementById('stop-button');
+const resetButton = document.getElementById('reset-button');
+
+startButton.addEventListener('click', startTimer);
+
+stopButton.addEventListener('click', stopTimer);
+
+resetButton.addEventListener('click', resetTimer);
+
+let interval;
+let timeLeft = 1500; //60seconds * 25minutes
+
+function updateTimer() {
+  let minute = Math.floor(timeLeft / 60);
+  let second = timeLeft % 60;
+  let formattedTime = `${minute.toString().padStart(2, '0')}:${second
+    .toString()
+    .padStart(2, '0')}`;
+
+  timer.textContent = formattedTime;
+}
+
+function startTimer() {
+  interval = setInterval(() => {
+    timeLeft--;
+    updateTimer();
+    if (timeLeft === 0) {
+      clearInterval(interval);
+      timeLeft = 1500;
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  alert('stop');
+}
+
+function resetTimer() {
+  alert('reset');
+}
